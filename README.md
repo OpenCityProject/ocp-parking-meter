8b669a2548c77ece4df826bdcd51af15f517620e
Wesley Yep <wesleyyep@hotmail.com>
2016-12-06 08:40:54 +1100

    tidied up code and updated README

--- README.md
+++ README.md
@@ -2,5 +2,16 @@ ocp-parking-meter
 ===================
 Contains code that will run on parking meter. It should integrate with the parking meter peripherals and send requests to web service.
 
-## Instructions
-- `python script.py` and follow prompts 
+## Instructions and use case
+- `python script.py` 
+- Choose the category of interest by entering a number on the keyboard
+- Hit enter
+- Choose a place of interest by entering a number on the keyboard
+- Hit enter
+
+## TODO
+- integrate with webservice to get actual categories
+- integrate with webservice to get actual POIs
+- integrate with parking meter buttons
+- integrate with parking meter printer
+- integrate with parking meter LCD
--- script.py
+++ script.py
@@ -1,44 +1,49 @@
 #!/usr/bin/python
 import sys
 
-def send_request(category, latitude, longitude):
-    # just print instead of sending get request for now
-    print "Sending request with parameters {category: " + category + ", latitude: " + \
-        str(latitude) + ", longitude: " + str(longitude) + "}"
-    # await response from server
-    poi_list = ["Cathedral Square", "Cantebury Museum", "Botanic Gardens"] # mock response for now
-    return poi_list
+class ParkingMeter:
+    categories = ["Get Back to Nature", "Within 2km", "Give Back to Community", "Today only", "Weekly"]
 
-def display(text):
-    # for now just print to console
-    print text
+    def send_request(self, category, latitude, longitude):
+        # just print instead of sending get request for now
+        print "Sending request with parameters {{ category: {0}, latitude: {1}, longitude: {2} }}".format(category, latitude, longitude)
+        # await response from server
+        poi_list = ["Cathedral Square", "Cantebury Museum", "Botanic Gardens"] # mock response for now
+        return poi_list
 
-def get_choice():
-    return input("Your choice: ") # change this to receive input from buttons on parking machine
+    def display(self, text):
+        # for now just print to console
+        print text
 
-def print_ticket(poi):
-    print "Printing ticket for " + poi
+    def get_choice(self):
+        return input("Your choice: ") # change this to receive input from buttons on parking machine
 
-def make_poi_selection(poi_list):
-    for i in range(0, len(categories)):
-        display(str(i+1) + ") " + poi_list[i])
-    sys.stdout.flush()
-    display("Please choose an option: ")
-    choice = get_choice()
-    if choice > 0 and choice <= len(poi_list):
-        print_ticket(poi_list[choice-1])
-    else:
-        display("Sorry, invalid choice")
+    def print_ticket(self, poi):
+        print "Printing ticket for " + poi
 
-categories = ["Parks", "Gardens", "Sports fields"]
-display("Welcome to Open City! Please choose a category and hit enter:")
-for i in range(0, len(categories)):
-    display(str(i+1) + ") " + categories[i])
-sys.stdout.flush()
-choice = get_choice()
-if choice > 0 and choice <= len(categories):
-    print "You chose " + categories[choice-1] # for debugging
-    poi_list = send_request(categories[choice-1], 0.00, 0.00)
-    make_poi_selection(poi_list)
-else:
-    display("Sorry, you chose an invalid category")
+    def make_poi_selection(self, poi_list):
+        for index, item in enumerate(poi_list, start = 1):
+            self.display("{0}) {1}".format(index, item))
+        sys.stdout.flush()
+        self.display("Please choose an option: ")
+        choice = self.get_choice()
+        if choice > 0 and choice <= len(poi_list):
+            self.print_ticket(poi_list[choice-1])
+        else:
+            self.display("Sorry, invalid choice")
+
+    def start(self):
+        self.display("Welcome to Open City! Please choose a category and hit enter:")
+        for index, item in enumerate(self.categories, start = 1):
+            self.display("{0}) {1}".format(index, item))
+        sys.stdout.flush()
+        choice = self.get_choice()
+        if choice > 0 and choice <= len(self.categories):
+            print "You chose " + self.categories[choice-1] # for debugging
+            poi_list = self.send_request(self.categories[choice-1], 0.00, 0.00)
+            self.make_poi_selection(poi_list)
+        else:
+            self.display("Sorry, you chose an invalid category")
+
+instance = ParkingMeter()
+instance.start()
