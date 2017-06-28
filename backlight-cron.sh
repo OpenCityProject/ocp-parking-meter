54a0bd40758401aa8ee1a5210964cba2c8c5a5b0
Wesley <wesleyyep@hotmail.com>
2017-06-28 16:53:16 +1200

    script rebooting cron

--- backlight-cron.sh
+++ backlight-cron.sh
@@ -1,8 +1,10 @@
+#! /bin/bash
+
 #numberPythonPrograms=$(ps aux | grep -c python)
 #if [ "$numberPythonPrograms" != "1" ]
 #then
 #    echo "Running"
 #else
 #    echo "Stopped"
-    echo -e "\xFE\x46" > /dev/ttyACM0
+echo -e $"\xFE\x46" > /dev/ttyACM0
 #fi
