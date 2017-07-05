af5364fee6a512085fcafcfeefd96ffdb40c2c7d
Wesley <wesleyyep@hotmail.com>
2017-07-05 21:03:37 +1200

    data load script

--- data-loader-cron.sh
+++ data-loader-cron.sh
@@ -3,8 +3,11 @@
 # first kill script running
 killall -9 python
 
+# change dir
+cd /home/pi/ocp-parking-meter
+
 # now call python script to load data
-python data-collector.py
+python /home/pi/ocp-parking-meter/data-collector.py
 
 # now reboot script
-./script-rebooter-cron.sh
+/home/pi/ocp-parking-meter/script-rebooter-cron.sh
